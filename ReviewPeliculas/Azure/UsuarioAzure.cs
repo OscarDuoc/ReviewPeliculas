ea5ba95e85c4e17221fec601401b74292e31cdb3
Oscar Poblete <os.pobleteq@alumnos.duoc.cl>
2020-11-27 17:41:19 -0300

    Se crean metodos AgregarUsuario con y sin parametros, EliminarUsuarios con y sin parametros y ActualizarUsuario por parametros

--- ReviewPeliculas/Azure/UsuarioAzure.cs
+++ ReviewPeliculas/Azure/UsuarioAzure.cs
@@ -49,9 +49,187 @@ namespace ReviewPeliculas.Azure
         private static SqlCommand obtenerUserPorNombres(SqlConnection connection, string nombres)
         {
             SqlCommand sqlCommand = new SqlCommand(null, connection);
-            sqlCommand.CommandText = $"select * from Administrador where nombres = '{nombres}'";
+            sqlCommand.CommandText = $"select * from Usuario where nombres = '{nombres}'";
             connection.Open();
             return sqlCommand;
+        }
+
+        public static Usuario obtenerUserPorApellidos(string apellidos)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var comando = obtenerUserPorApellidos(connection, apellidos);
+
+                var dataTable = LlenarDataTable(comando);
+
+                return CreacionUser(dataTable);
+
+            }
+        }
+
+        private static SqlCommand obtenerUserPorApellidos(SqlConnection connection, string apellidos)
+        {
+            SqlCommand sqlCommand = new SqlCommand(null, connection);
+            sqlCommand.CommandText = $"select * from Usuario where apellidos = '{apellidos}'";
+            connection.Open();
+            return sqlCommand;
+        }
+
+        public static Usuario obtenerUserPorGenero(string genero)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var comando = obtenerUserPorGenero(connection, genero);
+
+                var dataTable = LlenarDataTable(comando);
+
+                return CreacionUser(dataTable);
+
+            }
+        }
+
+        private static SqlCommand obtenerUserPorGenero(SqlConnection connection, string genero)
+        {
+            SqlCommand sqlCommand = new SqlCommand(null, connection);
+            sqlCommand.CommandText = $"select * from Usuario where genero = '{genero}'";
+            connection.Open();
+            return sqlCommand;
+        }
+
+        public static int AgregarUsuario(Usuario user)
+        {
+            int resultado = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(null, connection);
+                sqlCommand.CommandText = "Insert into Usuario (nombres,apellidos,edad,genero,email) values (@nombres,@apellidos,@edad,@genero,@email)";
+                sqlCommand.Parameters.AddWithValue("@nombres", user.nombres);
+                sqlCommand.Parameters.AddWithValue("@apellidos", user.apellidos);
+                sqlCommand.Parameters.AddWithValue("@edad", user.edad);
+                sqlCommand.Parameters.AddWithValue("@genero", user.genero);
+                sqlCommand.Parameters.AddWithValue("@email", user.email);
+
+                try
+                {
+                    connection.Open();
+                    resultado = sqlCommand.ExecuteNonQuery();
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return resultado;
+        }
+
+        public static int AgregarUsuario(string nombres, string apellidos, int edad, string genero, string email)
+        {
+            int resultado = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(null, connection);
+                sqlCommand.CommandText = "Insert into Usuario (nombres,apellidos,edad,genero,email) values (@nombres,@apellidos,@edad,@genero,@email)";
+                sqlCommand.Parameters.AddWithValue("@nombres", nombres);
+                sqlCommand.Parameters.AddWithValue("@apellidos", apellidos);
+                sqlCommand.Parameters.AddWithValue("@edad", edad);
+                sqlCommand.Parameters.AddWithValue("@genero", genero);
+                sqlCommand.Parameters.AddWithValue("@email", email);
+
+
+                try
+                {
+                    connection.Open();
+                    resultado = sqlCommand.ExecuteNonQuery();
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+            }
+            return resultado;
+        }
+
+        public static int EliminarUsuarioPorNombre(string nombres)
+        {
+            int resultado = 1;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(null, connection);
+                sqlCommand.CommandText = "Delete from Usuario where nombres = @nombres";
+                sqlCommand.Parameters.AddWithValue("@nombres", nombres);
+
+                try
+                {
+                    connection.Open();
+                    resultado = sqlCommand.ExecuteNonQuery();
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                return resultado;
+            }
+        }
+
+        public static int EliminarUsuarioPorApellidos(string apellidos)
+        {
+            int resultado = 1;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(null, connection);
+                sqlCommand.CommandText = "Delete from Usuario where apellidos = @apellidos";
+                sqlCommand.Parameters.AddWithValue("@apellidos", apellidos);
+
+                try
+                {
+                    connection.Open();
+                    resultado = sqlCommand.ExecuteNonQuery();
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                return resultado;
+            }
+        }
+
+        public static int ActualizarUsuarioPorId(Usuario usuario)
+        {
+            int resultado = 0;
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(null, sqlConnection);
+                sqlCommand.CommandText = "Update Usuario SET Usuario = @nombres, apellidos = @apellidos where idUsuario = @idUsuario";
+
+                sqlCommand.Parameters.AddWithValue("@nombres", usuario.nombres);
+                sqlCommand.Parameters.AddWithValue("@apellidos", usuario.apellidos);
+                sqlCommand.Parameters.AddWithValue("@edad", usuario.edad);
+                sqlCommand.Parameters.AddWithValue("@genero", usuario.genero);
+                sqlCommand.Parameters.AddWithValue("@email", usuario.email);
+
+                try
+                {
+                    sqlConnection.Open();
+                    resultado = sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return resultado;
         }
 
         private static Usuario CreacionUser(DataTable dataTable)
--- XUnitTestApiReviesPeliculas/UnitTestAdmins.cs
+++ XUnitTestApiReviesPeliculas/UnitTestAdmins.cs
@@ -50,5 +50,154 @@ namespace XUnitTestApiReviesPeliculas
             //Assert 
             Assert.NotNull(userRetornado);
         }
+
+        [Fact]
+        public void TestObtenerUserPorApellidos()
+        {
+            //Arrange
+            string apellidos = "Molina Contreras";
+            Usuario userRetornado;
+
+            //Act
+            userRetornado = UsuarioAzure.obtenerUserPorApellidos(apellidos);
+
+            //Assert 
+            Assert.NotNull(userRetornado);
+        }
+
+        [Fact]
+        public void TestObtenerUserPorGenero()
+        {
+            //Arrange
+            string genero = "Femenino";
+            Usuario userRetornado;
+
+            //Act
+            userRetornado = UsuarioAzure.obtenerUserPorGenero(genero);
+
+            //Assert 
+            Assert.NotNull(userRetornado);
+        }
+
+        [Fact]
+        public void TestAgregarUsuario()
+        {
+            //Arrange
+            int resultadoEsperado = 0;
+            int resultadoObtenido = 1;
+            Usuario user = new Usuario();
+            user.nombres = "Carlos Alberto ";
+            user.apellidos = "Vibes Bueno";
+            user.edad = 26;
+            user.genero = "Masculino";
+            user.email = "cvibes@gmail.com";
+
+            //Act
+            resultadoObtenido = UsuarioAzure.AgregarUsuario(user);
+
+            //Assert 
+            Assert.Equal(resultadoEsperado, resultadoObtenido);
+
+        }
+
+        [Fact]
+        public void TestAgregarUsuarioPorParametro()
+        {
+            //Arrange
+            int resultadoEsperado = 0;
+            int resultadoObtenido = 1;
+            string nombres = "Ramon Alberto";
+            string apellidos = "Gonzalez Tapia";
+            int edad =34;
+            string genero = "Masculino";
+            string email = "rgonzalez@gmail.com";
+
+            //Act
+            resultadoObtenido = UsuarioAzure.AgregarUsuario(nombres, apellidos,edad,genero,email);
+
+            //Assert 
+            Assert.Equal(resultadoEsperado, resultadoObtenido);
+
+        }
+
+        [Fact]
+        public void TestEliminarUsuarioPorNombre()
+        {
+            //Arrange         
+            Usuario user = new Usuario();
+            user.nombres = "Marco Andre";
+            user.apellidos = "Carmona Dominguez";
+            user.edad = 40;
+            user.genero = "Masculino";
+            user.email = "mcarmona@gmail.com";
+
+
+            string nombreUsuarioEliminar = "Marco Andre";
+
+            int resultadoEsperado = 1;
+            int resultadoObtenido = 0;
+
+            UsuarioAzure.AgregarUsuario(user);
+
+            //Act
+            resultadoObtenido = UsuarioAzure.EliminarUsuarioPorNombre(nombreUsuarioEliminar);
+
+            //Assert 
+            Assert.Equal(resultadoEsperado, resultadoObtenido);
+
+        }
+
+        [Fact]
+        public void TestEliminarUsuarioPorApellido()
+        {
+            //Arrange         
+            Usuario user = new Usuario();
+            user.idUsuario =3;
+            user.nombres = "Carla Romina";
+            user.apellidos = "Fernandez Salas";
+            user.edad = 23;
+            user.genero = "Femenino";
+            user.email = "cherrera@gmail.com";
+
+
+            string apellidosUsuarioEliminar = "Herrera Salas";
+
+            int resultadoEsperado = 1;
+            int resultadoObtenido = 0;
+
+            UsuarioAzure.AgregarUsuario(user);
+
+            //Act
+            resultadoObtenido = UsuarioAzure.EliminarUsuarioPorApellidos(apellidosUsuarioEliminar);
+
+            //Assert 
+            Assert.Equal(resultadoEsperado, resultadoObtenido);
+
+        }
+
+        [Fact]
+        public void TestActualizarUsuarioPorId()
+        {
+            //Arrange
+            int resultadoEsperado = 0;
+            int resultadoObtenido = 1;
+
+            Usuario usuario = new Usuario();
+            usuario.idUsuario = 1;
+            usuario.nombres = "Marco Andre";
+            usuario.apellidos = "Carmona Dominguez";
+            usuario.edad =40;
+            usuario.genero = "Masculino";
+            usuario.email = "mcarmona@gmail.com";
+            //Act
+            resultadoObtenido = UsuarioAzure.ActualizarUsuarioPorId(usuario);
+
+            usuario.nombres = "Roberto Andres";
+            UsuarioAzure.ActualizarUsuarioPorId(usuario);
+
+            //Assert
+            Assert.Equal(resultadoEsperado, resultadoObtenido);
+        }
+
     }
 }
